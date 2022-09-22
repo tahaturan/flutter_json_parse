143fca124eb72704b1935ed39938dcb75b182647
Taha Turan <tahaaturann16@gmail.com>
2022-09-22 16:53:06 +0300

    Json parse islemleri Tek Nesne

--- lib/model/messages.dart
+++ lib/model/messages.dart
@@ -1,1 +1,17 @@
-class Messages {}
+class Messages {
+  int messageCode;
+  String messageContent;
+  Messages({
+    required this.messageCode,
+    required this.messageContent,
+  });
+
+  //*json parametresi bizden {"mesaj_kod" : 1,"mesaj_icerik" : "basarili"} bu sekilde bir veri beklemektedir
+//*eger boyle bir veri ektarabilirsek bize bu yapiyi parse ederek bir nesne verecektir.
+
+  factory Messages.fromJson(Map<String, dynamic> json) {
+    return Messages(
+        messageCode: json["mesaj_kod"] as int,
+        messageContent: json["mesaj_icerik"] as String);
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_json_parse/model/messages.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +11,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void messageParse() {
+    String strData =
+        '{"mesajlar" : {"mesaj_kod" : 1,"mesaj_icerik" : "basarili" } }';
+
+    var jsonData = json.decode(strData);
+    //*bu sekilde yuakridaki string verimizi json formatina donustirmus olduk
+
+    var jsonObject = jsonData["mesajlar"];
+
+    var message = Messages.fromJson(jsonObject);
+
+    debugPrint("Mesaj kodu : ${message.messageCode} ");
+    debugPrint("Mesaj icerik : ${message.messageContent} ");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    messageParse();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
